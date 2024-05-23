bdc742673f5664d88fbdfb4b0864a0efdc4420b0
sheroques <luizalexandre99@hotmail.com.br>
2024-05-23 14:48:20 -0300

    criacao de rotas e fragmentacao de pastas

---
+++ tbt/src/rotas.jsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom"
+import {Home} from './Pages/home'
+
+
+const router = createBrowserRouter([
+    {
+        element:<Layout/>,
+        children:[
+            {
+                
+            }
+        ]
+    }
+])
